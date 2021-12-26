cdb347b702ac4bc9380786d34465c7fc69e467a6
Firuz Murodov <fimurodov@gmail.com>
2021-12-26 21:23:39 +0500

    added methods

---
+++ pkg/wallet/service.go
@@ -0,0 +1,80 @@
+package wallet
+
+import (
+	"errors"
+	"github.com/FiruzMurodov/wallet/pkg/types"
+	"github.com/google/uuid"
+)
+
+var ErrPhoneRegistered = errors.New("phone already registred")
+var ErrAmountMustBePositive = errors.New("amount must be greater than zero")
+var ErrAccountNotFound = errors.New("account not found")
+var ErrNotEnoughtBalance = errors.New("account not enought balance")
+
+type Service struct {
+	nextAccountID int64
+	accounts      []*types.Account
+	payments      []*types.Payment
+}
+
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+
+	s.nextAccountID++
+	account := &types.Account{
+		ID:      s.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+
+	return account, nil
+}
+
+//Pay method
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+	var account *types.Account
+	for _, ac := range s.accounts {
+		if ac.ID == accountID {
+			account = ac
+			break
+		}
+	}
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+	if account.Balance < amount {
+		return nil, ErrNotEnoughtBalance
+	}
+	account.Balance -= amount
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID:        paymentID,
+		AccountID: accountID,
+		Amount:    amount,
+		Category:  category,
+		Status:    types.PaymentStatusInProgress,
+	}
+	s.payments = append(s.payments, payment)
+	return payment, nil
+}
+
+func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
+
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			return acc, nil
+		}
+
+	}
+
+	return nil, ErrAccountNotFound
+}
